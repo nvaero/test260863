dfc411e4cef4c5495d3f5ed863f708f33eedd671
Nathane <you@example.com>
2024-07-12 10:39:00 +0200

    modif2

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-// Sélectionnez le header
+
 const header = document.querySelector('header');
 
-// Variable pour suivre la position de défilement précédente
+
 let lastScrollTop = 0;
 
-// Écouteur d'événement pour le défilement de la page
+
 window.addEventListener('scroll', () => {
-    // Obtenez la position de défilement actuelle
+    
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    // Vérifiez la direction du défilement et si le défilement est important
+    
     if (scrollTop > lastScrollTop && scrollTop > 100) {
-        // Faites défiler vers le bas et défilement important, ajoutez la classe "scrolled" au header
+        
         header.classList.add('scrolled');
     } else {
-        // Faites défiler vers le haut ou défilement pas important, supprimez la classe "scrolled" du header
+        
         header.classList.remove('scrolled');
     }
 
-    // Mettez à jour la position de défilement précédente
+
     lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
 });
 
 
 
 
-// Sélectionnez le bouton du menu hamburger
+//menu de navigation
 const navToggle = document.querySelector('.nav-toggle');
 const navMenu = document.querySelector('nav.main-nav');
 
-// Ajoutez un écouteur d'événement au bouton du menu hamburger
+
 navToggle.addEventListener('click', () => {
-    // Basculez la classe 'active' sur le bouton du menu hamburger
+    
     navToggle.classList.toggle('active');
 
-    // Affichez ou masquez le menu de navigation
+    
     navMenu.classList.toggle('active');
 });
 
@@ -44,7 +44,7 @@ navToggle.addEventListener('click', () => {
 
 
 function initMap() {
-    var location = { lat: 48.8566, lng: 2.3522 }; // Example: Coordinates for Paris
+    var location = { lat: 48.8566, lng: 2.3522 }; 
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
         center: location
@@ -57,10 +57,10 @@ function initMap() {
 
 
 
-
+//animation defilement pages
 AOS.init({
-    duration: 1000, // Durée de l'animation en millisecondes
-    once: true, // Si true, l'animation ne se produit qu'une seule fois
+    duration: 1000, 
+    once: true, 
 });
 
 
@@ -70,15 +70,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
     navLinks.forEach(function(link) {
         link.addEventListener("click", function(event) {
-            event.preventDefault(); // Empêcher le chargement immédiat de la page
+            event.preventDefault(); 
             
-            // Ajouter une classe pour déclencher l'animation (ex. fade-out)
+            
             link.classList.add("fade-out");
 
-            // Attendre quelques millisecondes pour l'animation (ajuster selon besoin)
+            
             setTimeout(function() {
-                window.location.href = link.href; // Rediriger vers le lien après l'animation
-            }, 500); // 500 millisecondes (ajuster la durée d'animation si nécessaire)
+                window.location.href = link.href; 
+            }, 500); 
         });
     });
 });
